089229427b3f5d5df8ace326121ef17cbccf0eb3
dawidbieniek <dawid_b01@wp.pl>
2023-11-09 12:46:50 +0100

    Add Calculator class

---
+++ App.Test/CalculatorTests.cs
@@ -0,0 +1,21 @@
+namespace App.Test
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(1.5, 2.1, 3.6)]
+        public void Add_ShouldReturnSumOf2Numbers_NumbersAreDouble(float a, float b, float expected)
+        {
+            // Arrange
+            Calculator calculator = new(a, b);
+
+            // Act
+            double result = calculator.Add();
+
+            // Assert
+            Assert.Equals(expected, result);
+        }
+    }
+}
---
+++ App/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App;
+
+public class Calculator
+{
+    private float _a;
+    private float _b;
+
+    public Calculator(float a, float b)
+    {
+        _a = a;
+        _b = b;
+    }   
+
+    public float Add()
+    {
+        return _a + _b;
+    }
+}
